a5eeeec8ef5b5788060d54062a3e60d78d770f24
Fhurai <kulu57@gmail.com>
2024-10-17 18:47:50 +0200

    Rename index page Background color added Comments + Generalization + global values

--- test/index.js
+++ test/index.js
@@ -1,25 +1,69 @@
+/**
+ * Initialization variables.
+ */
 const pourcentageContainer = document.querySelector(".pourcentage");
+let values = 0;
+let keys = 0;
 
+/**
+ * Event load page
+ */
 document.addEventListener("DOMContentLoaded", function (event) {
+    // Stats fandoms.
     fandoms()
         .then((fandoms) => {
-            pourcentageContainer.values = fandoms[0];
-            pourcentageContainer.keys = fandoms[1];
+            values += fandoms[0];
+            keys += fandoms[1];
+            drawPourcentage();
+        });
+
+    // Stats authors.
+    authors()
+        .then((authors) => {
+            values += authors[0];
+            keys += authors[1];
             drawPourcentage();
         });
 });
 
-function drawPourcentage(){
+/**
+ * Draw pourcentage bar from numbers got from all tests.
+ */
+function drawPourcentage() {
     const pourcentageBar = document.createElement("div");
     pourcentageBar.classList = "result";
-    pourcentageBar.innerHTML = (pourcentageContainer.values / pourcentageContainer.keys * 100) + "%";
-    pourcentageBar.style.width = (pourcentageContainer.values / pourcentageContainer.keys * 90) + "%"
+    pourcentageBar.innerHTML = Math.round(values / keys * 100) + "%";
+    pourcentageBar.style.width = (values / keys * 90) + "%"
     pourcentageContainer.appendChild(pourcentageBar);
 }
 
+/**
+ * Gets the fandoms tests numbers.
+ * @returns [numbers of successful tests, numbers of total tests]
+ */
 async function fandoms() {
+    const url = "testFandom.php";
+    return fetchTests(url);
+}
+
+/**
+ * Gets the authors tests numbers.
+ * @returns [numbers of successful tests, numbers of total tests]
+ */
+async function authors() {
+    const url = "testAuthor.php";
+    return fetchTests(url);
+}
+
+/**
+ * Gets the tests number in a given url.
+ * @param {string} url 
+ * @returns [numbers of successful tests, numbers of total tests]
+ */
+async function fetchTests(url) {
     try {
-        const response = await fetch("testFandom.php", {
+        // fetch call
+        const response = await fetch(url, {
             method: "GET",
             headers: {
                 "Content-type": "application/json"
@@ -27,15 +71,29 @@ async function fandoms() {
         });
 
         if (!response.ok)
+            // Fetch unsuccessfull, throw error.
             throw new Error(`Response status: ${response.status}`);
         else {
+            // Fetch successfull, manage json.
             const tests = await response.json();
-            if(Object.values(tests).length / Object.keys(tests).length === 1)
-                document.querySelector("a[href='testFandom.php']").classList = "success";
-            return [Object.values(tests).length, Object.keys(tests).length];
+
+            // Gets successful and total tests numbers.
+            let values = Object.values(tests).filter((result) => result === true).length;
+            let keys = Object.keys(tests).length;
+
+            if (values / keys === 1)
+                // If all tests are successfull, url is success
+                document.querySelector("a[href='" + url + "']").classList = "success";
+            else
+                // Not all tests are successfull, url is fail.
+                document.querySelector("a[href='" + url + "']").classList = "fail";
+
+            // Returns the numbers in an array.
+            return [values, keys];
         }
     } catch (error) {
-        console.log(error.message);
+        // Error catched, show error message + returns false.
+        console.error(error.message);
         return false;
     }
 }
--- test/index.php
+++ test/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index test folders</title>
+    <title>Tests Webservices FFB</title>
     <link rel="stylesheet" href="index.css">
 </head>
 
